d03612d6f4633a31d7d59c221a7323be7d9c5f4f
Eeshan Agarwal <eeshanagarwal@berkeley.edu>
2015-06-30 15:27:02 -0700

    playing with sending back sms1

--- hello.py
+++ hello.py
@@ -13,12 +13,24 @@ callers = {
 def hello_monkey():
     """Respond and greet the caller by name."""
  
+ 	counter = session.get('counter', 0)
+ 
+    # increment the counter
+    counter += 1
+ 
+    # Save the new counter value in the session
+    session['counter'] = counter
+
     from_number = request.values.get('From', None)
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
     else:
         message = "Bro, thanks for the message!"
- 
+ 	
+ 	if(counter > 1)
+ 	{
+ 		message = "Enter your symptoms: "
+ 	}
     resp = twilio.twiml.Response()
     resp.message(message)
  
