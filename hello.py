3740d5ada5f6a7bfc8fa5490cc801066c19a0805
Eeshan Agarwal <eeshanagarwal@berkeley.edu>
2015-06-30 15:45:43 -0700

    hey

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 
 from flask import Flask, request, redirect
 import twilio.twiml
- 
+import csv
+import json
+import re
+from pprint import pprint
+from collections import Counter
+from twilio.rest import TwilioRestClient
+
+
 app = Flask(__name__)
+ # The session object makes use of a secret key.
+SECRET_KEY = 'a secret key'
+app = Flask(__name__)
+app.config.from_object(__name__)
  
 # Try adding your own number to this list!
 callers = {
@@ -10,31 +21,44 @@ callers = {
 }
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond and greet the caller by name."""
- 
- 	counter = session.get('counter', 0)
- 
-    # increment the counter
-    counter += 1
- 
-    # Save the new counter value in the session
-    session['counter'] = counter
 
-    from_number = request.values.get('From', None)
-    if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
-    else:
-        message = "Bro, thanks for the message!"
- 	
- 	if(counter > 1)
- 	{
- 		message = "Enter your symptoms: "
- 	}
-    resp = twilio.twiml.Response()
-    resp.message(message)
+def search_disease(symptoms):
+    with open('finalDB.json') as data_file:    
+        data = json.load(data_file)
+    matchingDs = []
+
+    for aSymptom in symptoms:
+        
+        for x in data:
+            symsm = x['Symptom'].split(",")
+
+            for s in symsm:
+                if aSymptom in s:
+                    matchingDs.append(x['Disease'])
+               
+    
+    counterC = Counter(matchingDs)
+    return str(counterC.most_common(3))    
+        
+
+
+def hello_monkey():
+	body = request.values.get('Body', None)
+	message = search_disease(body)
+
+	r = twiml.Response()
+	r.message = message
+	return str(r)
+
  
-    return str(resp)
+  #   from_number = request.values.get('From', None)
+  #   if from_number in callers:
+  #       message = callers[from_number] + ", thanks for the message!"
+  #   else:
+  #       message = "Bro, thanks for the message!"
+ 	
+ 	# if counter > 0:
+	
  
 if __name__ == "__main__":
     app.run(debug=True)
