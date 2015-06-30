dd8c435d599d7d56c6184b378fa178ca195cd74c
Eeshan Agarwal <eeshanagarwal@berkeley.edu>
2015-06-30 15:55:16 -0700

    hey

--- hello.py
+++ hello.py
@@ -1,16 +1,35 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import json
+from collections import Counter
  
 app = Flask(__name__)
  
 # Try adding your own number to this list!
 callers = {
-    "+14158675309": "Curious George",
-    "+14158675310": "Boots",
-    "+14158675311": "Virgil",
+    "+16508669177": "Eeshan",
 }
  
 @app.route("/", methods=['GET', 'POST'])
+
+def search_disease(symptoms):
+    with open('finalDB.json') as data_file:    
+        data = json.load(data_file)
+    matchingDs = []
+
+    for aSymptom in symptoms:
+        
+        for x in data:
+            symsm = x['Symptom'].split(",")
+
+            for s in symsm:
+                if aSymptom in s:
+                    matchingDs.append(x['Disease'])
+               
+    
+    counterC = Counter(matchingDs)
+    return str(counterC.most_common(3)) 
+
 def hello_monkey():
     """Respond and greet the caller by name."""
  
@@ -19,7 +38,10 @@ def hello_monkey():
         message = callers[from_number] + ", thanks for the message!"
     else:
         message = "Monkey, thanks for the message!"
- 
+ 	
+ 	body = request.values.get('Body', None)
+	message = search_disease(body)
+
     resp = twilio.twiml.Response()
     resp.message(message)
  
@@ -27,3 +49,5 @@ def hello_monkey():
  
 if __name__ == "__main__":
     app.run(debug=True)
+
+
