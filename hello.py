938ad5bd0853c2b4b40ca1ee01a5df07e8dff8cd
Eeshan Agarwal <eeshanagarwal@berkeley.edu>
2015-06-30 16:11:00 -0700

    hey

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect
 import twilio.twiml
-import json
-from collections import Counter
+# import json
+# from collections import Counter
  
 app = Flask(__name__)
  
