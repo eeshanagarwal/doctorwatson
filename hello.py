6bfdd05eed7f255a3021a168ffc813186a0816a5
Eeshan Agarwal <eeshanagarwal@berkeley.edu>
2015-06-30 15:21:51 -0700

    playing with sending back sms

--- hello.py
+++ hello.py
@@ -1,14 +1,27 @@
+
 from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
  
+# Try adding your own number to this list!
+callers = {
+    "+16508669177": "Eeshan",
+}
+ 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond and greet the caller by name."""
+ 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Bro, thanks for the message!"
  
     resp = twilio.twiml.Response()
-    resp.message("Hello, Doctor Watson")
+    resp.message(message)
+ 
     return str(resp)
  
 if __name__ == "__main__":
